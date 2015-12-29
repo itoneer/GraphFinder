55ff28cf96ee2c99fa58f6953f105afe975ecb3e
itoneer <skamin26@gmail.com>
2015-12-29 17:30:53 +0100

    added a simple interface, done for today

---
+++ GraphFinder/src/graphfinder/algorithms/shortestRoad.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphfinder.algorithms;
+
+import graphfinder.adjlist.ALVertex;
+import java.util.List;
+
+/**
+ *
+ * @author HP
+ */
+public interface shortestRoad {
+    public List road(ALVertex a, ALVertex b);
+}
